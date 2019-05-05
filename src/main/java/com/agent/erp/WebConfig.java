89cfdbf69873081e3df2176a967cc7deeb22c85a
hongchenxu01 <3398905445@qq.com>
2019-05-05 23:05:17 +0800

    登陆

--- src/main/java/com/agent/erp/WebConfig.java
+++ src/main/java/com/agent/erp/WebConfig.java
@@ -1,30 +1,55 @@
 package com.agent.erp;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
 public class WebConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
+		http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+			.and()
+			.authorizeRequests()
 	        .antMatchers("/", "/static/**").permitAll()
 	        .antMatchers("/admin/**","/upload/**").hasAnyRole("ADMIN")
-	        .antMatchers("/order/**").hasAnyRole("USER","ADMIN")
 	        .anyRequest().authenticated()
 	        .and()
 	        .formLogin()
 	        .loginPage("/login").permitAll()
+	        .usernameParameter("username")
+	        .passwordParameter("password")
+	        .defaultSuccessUrl("/")
+//	        .successHandler(successHandler())
+	        .failureUrl("/login?error=LOGIN_FAILED").permitAll()
+	        
 	        .and()
 	        .logout().permitAll();
 //	        .and()
 //	        .exceptionHandling().accessDeniedHandler(accessDeniedHandler);
 	}
 
-	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(this.userDetailsService).passwordEncoder(passwordEncoder());
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 	
 }
