b32c11f9d0550db1b005b6cce27d28179631c966
hongchenxu01 <3398905445@qq.com>
2019-05-05 23:06:10 +0800

    登陆

--- src/main/java/com/agent/erp/controller/HomeController.java
+++ src/main/java/com/agent/erp/controller/HomeController.java
@@ -1,11 +1,13 @@
 package com.agent.erp.controller;
 
 import java.util.Date;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.agent.erp.model.User;
 import com.agent.erp.service.UserService;
@@ -18,6 +20,20 @@ public class HomeController extends BaseController{
 	private UserService userService;
 	
 	@RequestMapping("")
+	public String root(Model model) {
+		
+		User u = new User();
+		u.setName("xhc2");
+		u.setPassword("123456");
+		u.setCreateTime(new Date());
+		
+//		String savedUserId = userService.save(u);
+		model.addAttribute("user", u);
+		return "index";
+		
+	}
+	
+	@RequestMapping("index")
 	public String index(Model model) {
 		
 		User u = new User();
@@ -32,7 +48,13 @@ public class HomeController extends BaseController{
 	}
 	
 	@RequestMapping("/login")
-	public String login(Model model) {
+	public String login(@RequestParam Optional<String> error, Model model) {
+		
+		if(error.isPresent()) {
+			model.addAttribute("error", error.get());
+		}
+		
+		
 		return "login";
 	}
 }
